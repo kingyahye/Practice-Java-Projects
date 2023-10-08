a1dc94f0124e84d299e7391d5df23f29b7fe21a8
kingyahye <yahye.hassan44@gmail.com>
2023-10-08 09:56:02 -0500

    First Commit

---
+++ 01-Color Flipper/app.js
@@ -0,0 +1,16 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
+
+btn.addEventListener("click",function(){
+  // get ranNum btw 0 - 3
+  const randomNumber = getRandomNumber();  
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+//arrow function
+const getRandomNumber = () => Math.floor(Math.random() * colors.length);
+
+
+
